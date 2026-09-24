fa14c05b7b6d496d53d4d9037c54d44fb7d1b741
Dnyaneshwarmule01 <dnyaneshwarmule49@gmail.com>
2026-09-24 20:17:34 +0530

    upload

---
+++ Greater_Number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int number1 , number2;
+    printf("Enter number1 : ");
+    scanf("%d",&number1);
+    printf("Enter number2 : ");
+    scanf("%d",&number2);
+    if(number1>number2){
+        printf("number1 is grater");
+    }
+}
